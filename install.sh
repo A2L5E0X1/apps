8c9bf89bbd79bf8a89cec8ce81e44c842cd6266e
A2L5E0X1 <gameglitscher@linux4.de>
2020-10-30 16:15:42 +0100

    apps: add apks and installer

---
+++ install.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+#
+# This Script is by A2L5E0X1
+#
+
+# Aurora Store - OpenSource Google Play Alternative
+adb install ./apps/Aurora.apk
+
+# FDroid - Free and OpenSource Apps
+adb install ./apps/FDroid.apk
+
+# K9Mail - OpenSource E-Mail App
+adb install ./apps/K9Mail.apk
+
+# Lightning - Light OpenSource Webbrowser
+adb install ./apps/Lightning.apk
+
+# Material Files - OpenSource File Manager
+adb install ./apps/Files.apk
+
+# Metro - OpenSource Music Player (RetroMusic Fork)
+adb install ./apps/Metro.apk
+
+# NewPipe - OpenSource YouTube Client
+adb install ./apps/NewPipe.apk
+
+# OpenBoard - OpenSource Keyboard
+adb install ./apps/OpenBoard.apk
+
+# QKSMS - OpenSource SMS Client
+adb install ./apps/QKSMS.apk
+
+# Simple Gallery - OpenSource Gallery App
+adb install ./apps/Gallery.apk
+
+echo "All Apps are upgradeable in F-Droid"
+echo "Have fun with your FOSS Apps :)"
